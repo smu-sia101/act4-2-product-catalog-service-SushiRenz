256d307f6edbacc4a7b937c54a965e38cf376223
SushiRenz <renz09358@gmail.com>
2025-04-16 15:51:10 +0800

    done API (Still no UI)

---
+++ ProductCatalogRamos/ProductCatalogRamos/Controllers/ProductConnectServerController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductCatalogServiceRamos.ProductModels;
+using ProductCatalogServiceRamos.Services;
+
+namespace ProductCatalogServiceRamos.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductConnectServerController : Controller
+    {
+        private readonly ProductsService _productsServices;
+
+        public ProductConnectServerController(ProductsService ProductsServices)
+        {
+            _productsServices = ProductsServices;
+        }
+
+        [HttpGet]
+        public async Task<List<Products>> Get() =>
+        await _productsServices.GetAsync();
+
+        [HttpGet("{id:length(24)}")]
+        public async Task<ActionResult<Products>> Get(string id)
+        {
+            var product = await _productsServices.GetAsync(id);
+
+            if (product is null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Products newProducts)
+        {
+            await _productsServices.CreateAsync(newProducts);
+
+            return CreatedAtAction(nameof(Get), new { id = newProducts.Id }, newProducts);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public async Task<IActionResult> Update(string id, Products updatedProducts)
+        {
+            var product = await _productsServices.GetAsync(id);
+
+            if (product is null)
+            {
+                return NotFound();
+            }
+
+            updatedProducts.Id = product.Id;
+
+            await _productsServices.UpdateAsync(id, updatedProducts);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var product = await _productsServices.GetAsync(id);
+
+            if (product is null)
+            {
+                return NotFound();
+            }
+
+            await _productsServices.RemoveAsync(id);
+
+            return NoContent();
+        }
+
+    }
+}
---
+++ ProductCatalogRamos/ProductCatalogRamos/Services/ProductServices.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using ProductCatalogService.ProductModels;
+using ProductCatalogServiceRamos.ProductModels;
+
+namespace ProductCatalogServiceRamos.Services;
+
+public class ProductsService
+{
+    private readonly IMongoCollection<Products> _productsCollection;
+
+    public ProductsService(
+        IOptions<ProductsDatabaseSettings> productsDatabaseSettings)
+    {
+        var mongoClient = new MongoClient(
+            productsDatabaseSettings.Value.ConnectionString);
+
+        var mongoDatabase = mongoClient.GetDatabase(
+            productsDatabaseSettings.Value.DatabaseName);
+
+        _productsCollection = mongoDatabase.GetCollection<Products>(
+            productsDatabaseSettings.Value.ProductsCollectionName);
+    }
+
+    public async Task<List<Products>> GetAsync() =>
+     await _productsCollection.Find(_ => true).ToListAsync();
+
+    public async Task<Products?> GetAsync(string id) =>
+        await _productsCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+    public async Task CreateAsync(Products newProducts) =>
+        await _productsCollection.InsertOneAsync(newProducts);
+
+    public async Task UpdateAsync(string id, Products updatedProducts) =>
+        await _productsCollection.ReplaceOneAsync(x => x.Id == id, updatedProducts);
+
+    public async Task RemoveAsync(string id) =>
+        await _productsCollection.DeleteOneAsync(x => x.Id == id);
+}
